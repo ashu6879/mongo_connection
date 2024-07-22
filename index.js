50ca8d16bdcc71268a62d2e6b53e270fcb82c2b1
ashu6879 <144018820+ashu6879@users.noreply.github.com>
2024-07-22 10:29:00 +0530

    index page

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ require('./src/config/db'); // Connect to MongoDB
 app.use(express.json());
 app.use('/', userRoutes);
 
-app.listen(3001, () => {
-  console.log('Server running on port 3001');
+app.listen(80, () => {
+  console.log('Server running on port 80');
 });
