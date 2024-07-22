27b7c336d88951f77658184b95acc9702f9487ab
ashu6879 <144018820+ashu6879@users.noreply.github.com>
2024-07-22 11:09:26 +0530

    index page

--- netlify/functions/getUsers.js
+++ netlify/functions/getUsers.js
@@ -1,14 +1,21 @@
 const { MongoClient } = require('mongodb');
 
-const uri = process.env.MONGODB_CONNECTION_STRING; // MongoDB connection string from environment variables
+const uri = process.env.MONGODB_URI;
 
 exports.handler = async function(event, context) {
+  console.log('Function start');
   try {
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('Connecting to MongoDB');
     await client.connect();
-    const database = client.db('alphadb');
-    const collection = database.collection('wp_users');
-    const users = await collection.find({}).limit(100).toArray();
+    console.log('Connected to MongoDB');
+
+    const database = client.db('yourDatabaseName');
+    const collection = database.collection('yourCollectionName');
+    console.log('Querying database');
+    const users = await collection.find({}).limit(50).toArray();
+
+    console.log('Query complete, closing connection');
     await client.close();
 
     return {
@@ -16,6 +23,7 @@ exports.handler = async function(event, context) {
       body: JSON.stringify(users),
     };
   } catch (error) {
+    console.error('Error occurred:', error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Failed to fetch users', details: error.message }),
